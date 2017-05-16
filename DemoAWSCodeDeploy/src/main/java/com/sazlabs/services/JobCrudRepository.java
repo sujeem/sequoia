e6329f602d3509459e0ae64d3c3a87e553ace652
sujeem <sujeem@192.168.0.4>
2017-05-15 23:28:41 -0400

    Removed

--- DemoAWSCodeDeploy/src/main/java/com/sazlabs/services/JobCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sazlabs.services;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sazlabs.controller.JobModel;
-
-public interface JobCrudRepository extends CrudRepository<JobModel, Long> {
-
-}
